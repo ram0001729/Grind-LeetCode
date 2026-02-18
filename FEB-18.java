547f980129e1d97577eceaadee8dd81c935326d5
RAMCHARAN <mekalaramcharan21@gmail.com>
2026-02-18 15:15:22 +0530

    Implement isValidBST method for binary search tree

---
+++ FEB-18.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static boolean BST(TreeNode root, long min, long max) {
+        if (root == null) return true;
+
+        if (root.val <= min || root.val >= max) return false;
+
+        return BST(root.left, min, root.val) &&
+               BST(root.right, root.val, max);
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return BST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+}
